411e6a4fdecd564f84337f3bc357cb45a2974432
Alexis Martinez <jhonyalexis100@gmail.com>
2024-07-17 10:15:24 -0500

    init project

---
+++ src/test/java/jagarcia/registration/login/system/RegistrationLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jagarcia.registration.login.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
