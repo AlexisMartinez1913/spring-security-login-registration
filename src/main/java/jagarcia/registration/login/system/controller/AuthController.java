559e848cdf34f24910e1570c44ffc04b797941db
Alexis Martinez <jhonyalexis100@gmail.com>
2024-07-17 11:49:18 -0500

    Create Handler Method to Handle Registration Form Request

--- src/main/java/jagarcia/registration/login/system/controller/AuthController.java
+++ src/main/java/jagarcia/registration/login/system/controller/AuthController.java
@@ -1,6 +1,8 @@
 package jagarcia.registration.login.system.controller;
 
+import jagarcia.registration.login.system.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,13 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+    //handle method to handle registration form request
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        //create model object to store form data
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
